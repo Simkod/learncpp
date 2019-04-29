b9456e5ec837f83cd501a52b4584eb13708e5d57
Simkod <45272799+Simkod@users.noreply.github.com>
2019-04-29 17:21:52 +0200

    c4e5

---
+++ Chapter 4/Chapter4-Exercises/Chapter4_Exercise5.cpp
@@ -0,0 +1,45 @@
+//Basic Calculator
+//4 basic math operations (* , / , + , -)
+//Two input values
+//Input: double double operator e.g.(24.5 33.2 +)
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //ask input
+    cout << "Basic calculator" << endl;
+    cout << "Please give your input as: firstnumber secondnumber operator, e.g. '24.5 33.2 +'"<< endl;
+    double firstNumber(0);
+    double secondNumber(0);
+    char mathOperator('\0');
+    cin >> firstNumber >> secondNumber >> mathOperator;
+
+
+    //Logic
+
+    double result(0);
+    switch (mathOperator)
+    {
+    case '+':
+        result = firstNumber + secondNumber;
+        break;
+    case '-':
+        result = firstNumber - secondNumber;
+        break;
+    case '*':
+        result = firstNumber * secondNumber;
+        break;
+    case '/':
+        result = firstNumber / secondNumber;
+        break;
+    default:
+        break;
+    }
+
+    //Output
+    cout << result << endl;
+
+    system("pause");
+}
