028a11ba90b4129a2d702eef5f8b0d15ea184571
Daniel Simko <45272799+Simkod@users.noreply.github.com>
2019-09-16 17:03:34 +0200

    Ch9_Ex6

---
+++ Chapter 9/Exercises/Chapter9_Exercise6/Library.cpp
@@ -0,0 +1,95 @@
+#include "Library.h"
+#include <assert.h>
+#include <iostream>
+
+//typedef std::pair<Book, bool> BookAndState;
+
+Library::Library()
+{
+}
+
+Library::~Library()
+{
+}
+
+void Library::checkInBook(const Book& bookToCheckIn)
+{
+    auto book = m_BooksAndStates.find(bookToCheckIn);
+
+    if (book->second == BookStatus::Out)
+    {
+        book->second = BookStatus::In;
+    }
+}
+
+void Library::checkOutBook(const Book& bookToCheckOut)
+{
+    auto book = m_BooksAndStates.find(bookToCheckOut);
+
+    if (book->second == BookStatus::In)
+    {
+        book->second = BookStatus::Out;
+    }
+}
+
+std::map<Book, Library::BookStatus, Library::comp> Library::getBooksAndStates() const
+{
+    return m_BooksAndStates;
+}
+
+void Library::addBook(Book bookToAdd)
+{
+    int originalNumberOfBooks = m_BooksAndStates.size();
+
+    std::pair<Book, BookStatus> bookAndState = std::make_pair(bookToAdd, BookStatus::In);
+    m_BooksAndStates.insert(bookAndState);
+
+    assert(originalNumberOfBooks + 1 == m_BooksAndStates.size() && "Book was not added");
+}
+
+void Library::printBookData() const
+{
+
+    std::cout << "Book# \t Author \t Title \t\t ISBN \t\t\t Copyright \t Status";
+    std::cout << "\n";
+
+    int counter(1);
+    for (std::map<Book, BookStatus, comp>::const_iterator it = m_BooksAndStates.begin(); it != m_BooksAndStates.end(); it++)
+    {
+        std::cout << "\n";
+        std::cout << "\n";
+        std::cout << "\n";
+        std::cout << it->first;
+        std::cout << "\n";
+        std::cout << "\n";
+        std::cout << "\n";
+
+
+        std::string bookState = std::string();
+        if (it->second == BookStatus::In)
+        {
+            bookState = "In";
+        }
+        else if (it->second == BookStatus::Out)
+        {
+            bookState = "Out";
+        }
+        else if (it->second == BookStatus::Unknown)
+        {
+            bookState = "Unknown";
+        }
+
+        std::cout << counter << "  " << "\t"
+                  << it->first.getAuthor() << "\t" 
+                  << it->first.getTitle() << "\t" 
+                  << it->first.getISBN().getISBNasString() << "\t"
+                  << it->first.getCopyrightDate().getCopyrightDateAsString() << "\t"
+                  << bookState;
+        std::cout << "\n";
+
+        ++counter;
+    }
+    std::cout << "\n";
+}
+
+
---
+++ Chapter 9/Exercises/Chapter9_Exercise6/Main.cpp
@@ -0,0 +1,28 @@
+//Chapter 9 Exercise 6
+
+#include "Library.h"
+#include "Date.h"
+
+void main()
+{
+    Library* niceLibrary = new Library();
+
+    Date copyrightDate1 = Date(1992, mar, 26);
+    Date copyrightDate2 = Date(1993, mar, 26);
+    Date copyrightDate3 = Date(1994, mar, 26);
+    Book ExampleBook1 = Book("978-3-16-148410-1", "Nice Title1", "Author Guy", copyrightDate1);
+    Book ExampleBook2 = Book("978-3-16-148410-2", "Nice Title2", "Author Guy", copyrightDate2);
+    Book ExampleBook3 = Book("978-3-16-148410-3", "Nice Title3", "Author Guy", copyrightDate3);
+
+    niceLibrary->addBook(ExampleBook1);
+    niceLibrary->addBook(ExampleBook2);
+    niceLibrary->addBook(ExampleBook3);
+
+    niceLibrary->printBookData();
+
+    niceLibrary->checkOutBook(ExampleBook2);
+
+    niceLibrary->printBookData();
+
+    system("pause");
+}
